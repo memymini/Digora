4d1126b52383298cdf5e278b758c6920c0d19bb2
memymini <minhi0614@gmail.com>
2025-09-29 21:46:19 +0900

    feat: 카카오 로그인 로직 및 미들웨어 구현

---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        get: (name) => {
+          return request.cookies.get(name)?.value;
+        },
+        set: (name, value, options) => {
+          response.cookies.set(name, value, options);
+        },
+        remove: (name, options) => {
+          response.cookies.delete({ name, ...options });
+        },
+      },
+    }
+  );
+
+  await supabase.auth.getUser();
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
