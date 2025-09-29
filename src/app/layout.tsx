6072fc7d5568a557aecf585cc3d38a0fe66e061d
memymini <minhi0614@gmail.com>
2025-09-29 21:56:39 +0900

    feat: session provider 및 toaster 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,48 @@ import "./globals.css";
 import { Header } from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import { QueryProvider } from "./QueryProvider";
+import { createClient } from "@/lib/supabase/server";
+import { SessionProvider } from "./SessionProvider";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "디고라",
   description: "상시적 직접 민주주의의 가능성을 실험하는 소셜 폴링 플랫폼",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  let session = null;
+  if (user) {
+    const { data: profile } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user.id)
+      .single();
+
+    session = { user, profile };
+  }
+
   return (
     <html lang="ko">
       <body className="">
         <QueryProvider>
-          <Header />
-          {children}
-          <Footer />
+          <SessionProvider session={session}>
+            <Header />
+            {children}
+            <Footer />
+          </SessionProvider>
         </QueryProvider>
+        <Toaster />
       </body>
     </html>
   );
