ea4732792593907cc25a55201fa8b08b964cdac3
memymini <minhi0614@gmail.com>
2025-10-05 22:11:05 +0900

    chore: Next Image가 supabase image 허용하도록 변경

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "opgraecvzigbdyoctojq.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
         hostname: "**",
       },
     ],
