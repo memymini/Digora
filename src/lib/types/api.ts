53e2943414e8580854f22882fa730ad23213e6e1
memymini <minhi0614@gmail.com>
2025-10-10 22:16:32 +0900

    chore: api type 분리

---
+++ src/lib/types/api.ts
@@ -0,0 +1,12 @@
+export interface ApiResponse<T> {
+  success: boolean;
+  data: T;
+}
+
+export interface ApiErrorResponse {
+  success: false;
+  error: {
+    code: string;
+    message: string;
+  };
+}
