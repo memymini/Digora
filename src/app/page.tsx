1ace4f5c053afbeede6eef8fa9873f3bd65343a0
memymini <minhi0614@gmail.com>
2025-10-15 15:01:45 +0900

    style: 메인페이지 간격 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,16 +28,17 @@ export default function Home() {
   const { data: heroVote, isLoading } = useHeroVoteQuery();
   return (
     <div className="min-h-screen bg-background w-full">
-      <main className="container mx-auto px-6 pt-24 w-full gap-4 flex flex-col sm:gap-12 items-center">
+      <main className="container mx-auto px-6 pt-12 md:pt-24 w-full flex flex-col gap-12 md:gap-24 items-center">
         <h1 className="w-fit mb-2 text-3xl md:text-5xl font-black leading-tight text-center text-transparent bg-gradient-to-r from-vote-blue via-primary to-vote-red bg-clip-text ">
-          여론의 새로운 기준,
+          여론의 새로운 기준,&nbsp;
           <br className="sm:hidden" />
           디고라(Digital + Agora)
         </h1>
-        <p className="max-w-2xl mx-auto body-text text-muted-foreground md:body-text-1 text-center mb-8">
+        <p className="max-w-2xl mx-auto body-text text-muted-foreground md:body-text-1 text-center">
           온라인 커뮤니티는 <strong>신뢰</strong>를,
           <br className="sm:hidden" /> 전통 여론조사는
-          <strong>시대</strong>를 잃었습니다.
+          <strong> 시대</strong>
+          를 잃었습니다.
           <br />
           디고라에서 왜곡 없는 진짜 여론을 확인하세요.
         </p>
@@ -51,7 +52,7 @@ export default function Home() {
         )}
 
         {/* Active Votes Section */}
-        <section className="mt-24 w-full">
+        <section className="mt-12 w-full" id="vote">
           <div className="flex items-center justify-between mb-6">
             <h2 className="heading-2 sm:heading-1">실시간 인기 투표🔥</h2>
             <div className="flex items-center gap-2 caption-text text-muted-foreground">
@@ -59,7 +60,6 @@ export default function Home() {
               <span>실시간 업데이트</span>
             </div>
           </div>
-
           <VoteFeed />
         </section>
       </main>
