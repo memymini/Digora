ec247e53bc95321098276e5f40c8a3d0067c03a5
memymini <minhi0614@gmail.com>
2025-10-18 22:27:26 +0900

    del: 불필요 코드 삭제

--- src/hooks/mutations/useVoteMutation.ts
+++ src/hooks/mutations/useVoteMutation.ts
@@ -3,7 +3,6 @@ import { VOTE_QUERY_KEYS } from "../queries/querykeys";
 import toast from "react-hot-toast";
 import { http } from "@/lib/fetcher";
 import { useApiMutation } from "../useApiMutation";
-import { useRouter } from "next/navigation";
 
 export const submitVote = async ({
   voteId,
@@ -23,7 +22,6 @@ interface VoteMutationParams {
 
 export function useVoteMutation({ voteId }: VoteMutationParams) {
   const queryClient = useQueryClient();
-  //const router = useRouter();
   return useApiMutation(
     ({ optionId }: { optionId: number }) => submitVote({ voteId, optionId }),
     {
@@ -33,7 +31,6 @@ export function useVoteMutation({ voteId }: VoteMutationParams) {
           queryKey: VOTE_QUERY_KEYS.detail(voteId),
         });
         queryClient.invalidateQueries({ queryKey: VOTE_QUERY_KEYS.feed() });
-        // router.push(`/result/${voteId}`);
       },
     }
   );
